77847abda61b69b1602ce538cf9e6fdf5b69c97d
Dante <Dante@DESKTOP-SH607Q8>
2024-03-15 13:52:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string path = @"C:\Users\Dante\Desktop\1.txt";
+            string newPath = @"C:\Users\Dante\Desktop\2\1.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(destFileName: newPath);
+            }
+        }
+        
+    }
+}
